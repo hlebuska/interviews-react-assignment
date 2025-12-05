729fd956b7163528d020ee1bda786b9519775674
Gleb Vassyutinskiy <g.v.vass@gmail.com>
2025-12-06 00:55:06 +0500

    search params for categories

--- src/features/products/hooks/use-categories.tsx
+++ src/features/products/hooks/use-categories.tsx
@@ -6,13 +6,24 @@ interface useCategoriesProps {
 }
 
 export const useCategories = ({unfilteredProducts}: useCategoriesProps) => {
-  const [categories, setCategories] = useState<string[]>([]);
+  const params = new URLSearchParams(window.location.search);
+  const urlCategories = params.getAll("category");
+
+  const [categories, setCategories] = useState<string[]>(urlCategories || []);
 
   const toggleCategory = (category: string) => {  
+    const params = new URLSearchParams(window.location.search);
+
     setCategories((prevCategories) => {
       if (prevCategories.includes(category)) {
+        params.delete("category", category);
+        window.history.replaceState({}, "", `?${params.toString()}`); 
+
         return prevCategories.filter((cat) => cat !== category);
       } else {
+        params.append("category", category);
+        window.history.replaceState({}, "", `?${params.toString()}`); 
+
         return [...prevCategories, category];
       }
     });
@@ -21,6 +32,8 @@ export const useCategories = ({unfilteredProducts}: useCategoriesProps) => {
   const filteredProducts = unfilteredProducts?.filter(product => 
     categories.length === 0 || categories.includes(product.category)
   ) || [];
+
+
 
   return {
     categories,
